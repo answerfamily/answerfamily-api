04c95d85f57f0f1920a829a75fb6e42be5a6eda3
MrOrz <johnsonliang7@gmail.com>
2018-10-28 23:02:29 -0400

    MongoDB client & schema script

---
+++ db/schema/mongo.js
@@ -0,0 +1,37 @@
+require('../../src/lib/catchUnhandledRejection');
+
+const mongoClient = require('../../src/lib/mongoClient');
+
+const SCHEMA = {
+  paragraph_replies: {
+    bsonType: 'object',
+    description: 'n-m join table of paragraphs and replies',
+
+    required: ['paragraphId', 'replyId', 'createdAt'],
+    properties: {
+      paragraphId: {
+        bsonType: 'string',
+        description: 'Elasticsearch ID of connected paragraph',
+      },
+      replyId: {
+        bsonType: 'string',
+        description: 'Elasticsearch ID of connected reply',
+      },
+      createdAt: {
+        bsonType: 'date',
+      },
+    },
+  },
+};
+
+mongoClient.then(async ({ client, db }) => {
+  await Promise.all(
+    Object.keys(SCHEMA).map(indexName =>
+      db.createCollection(indexName, {
+        validator: { $jsonSchema: SCHEMA[indexName] },
+      })
+    )
+  );
+
+  client.close();
+});
--- src/lib/mongoClient.js
+++ src/lib/mongoClient.js
@@ -3,13 +3,12 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(process.env.MONGODB_URL);
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.error('Cannot connect to MongoDB:', err);
-  } else {
-    // eslint-disable-next-line no-console
-    console.log('Connected successfully to MongoDB');
-  }
+module.exports = client.connect().then(client => {
+  // eslint-disable-next-line no-console
+  console.log('Connected successfully to MongoDB');
+
+  return {
+    client,
+    db: client.db(process.env.MONGODB_DBNAME),
+  };
 });
