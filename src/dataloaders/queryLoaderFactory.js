85b4faf3f2deb75cb615d85d8c18faa129079bd2
MrOrz <johnsonliang7@gmail.com>
2018-10-29 18:19:03 +0900

    MongoDB key-value dataloader

---
+++ src/dataloaders/queryLoaderFactory.js
@@ -0,0 +1,34 @@
+const DataLoader = require('dataloader');
+const mongoClient = require('../lib/mongoClient');
+
+/**
+ * Given a MongoDB collection, query, key to specify ID, and the value,
+ * return all MongoDB document that whose key is the value
+ */
+module.exports = (loaders, collectionName, key = '_id') =>
+  new DataLoader(async values => {
+    const { db } = await mongoClient;
+    const results = await db
+      .collection(collectionName)
+      .aggregate([
+        {
+          $match: {
+            [key]: { $in: values },
+          },
+        },
+        {
+          $group: {
+            _id: `$${key}`,
+            docs: { $push: '$$ROOT' },
+          },
+        },
+      ])
+      .toArray();
+
+    return values.map(id => {
+      const result = results.find(({ _id }) => _id === id);
+      if (result)
+        return result.docs.map(({ _id, ...doc }) => ({ ...doc, id: _id }));
+      return null;
+    });
+  });
