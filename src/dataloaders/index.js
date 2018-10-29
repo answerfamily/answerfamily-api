ce1f0a97dd37bcdc660d55d46e1321daae0afed7
MrOrz <johnsonliang7@gmail.com>
2018-10-29 18:16:30 +0900

    Add parameter to dataloader helper

--- src/dataloaders/index.js
+++ src/dataloaders/index.js
@@ -1,5 +1,6 @@
 const docLoaderFactory = require('./docLoaderFactory');
 const searchResultLoaderFactory = require('./searchResultLoaderFactory');
+const queryLoaderFactory = require('./queryLoaderFactory');
 
 module.exports = class DataLoaders {
   // List of data loaders
@@ -15,16 +16,34 @@ module.exports = class DataLoaders {
     );
   }
 
+  get paragraphRepliesByParagraphIdLoader() {
+    return this._checkOrSetLoader(
+      'queryLoader',
+      queryLoaderFactory,
+      'paragraphReplies',
+      'paragraphId'
+    );
+  }
+
+  get paragraphRepliesByReplyIdLoader() {
+    return this._checkOrSetLoader(
+      'queryLoader',
+      queryLoaderFactory,
+      'paragraphReplies',
+      'replyId'
+    );
+  }
+
   // inner-workings
   //
   constructor() {
     this._loaders = {};
   }
 
-  _checkOrSetLoader(name, factoryFn) {
+  _checkOrSetLoader(name, factoryFn, ...args) {
     if (this._loaders[name]) return this._loaders[name];
 
-    this._loaders[name] = factoryFn(this);
+    this._loaders[name] = factoryFn(this, ...args);
     return this._loaders[name];
   }
 };
