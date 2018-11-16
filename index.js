e4529a9959ba6eb2c1f60518bf218b8994188293
MrOrz <johnsonliang7@gmail.com>
2018-11-16 22:07:16 +0800

    Init config for Auth0

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
+require('./src/lib/catchUnhandledRejection');
 const schema = require('./src/schema');
 const DataLoaders = require('./src/dataloaders');
 const { ApolloServer } = require('apollo-server');
+const getUser = require('./src/lib/auth');
+
 const PORT = process.env.PORT || 9000;
 
 const server = new ApolloServer({
   schema,
-  context: () => ({ loaders: new DataLoaders() }),
   introspection: true, // Allow introspection in production as well
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+
+    return {
+      loaders: new DataLoaders(),
+      user,
+    };
+  },
 });
 server
   .listen({
---
+++ src/lib/auth.js
@@ -0,0 +1,45 @@
+// Reference: https://auth0.com/blog/develop-modern-apps-with-react-graphql-apollo-and-add-authentication/
+//
+
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+
+function getKey(header, cb) {
+  client.getSigningKey(header.kid, function(err, key) {
+    if (err) {
+      throw new Error('Cannot get signing key');
+    }
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    cb(null, signingKey);
+  });
+}
+
+const options = {
+  audience: process.env.AUTH0_CLINET_ID,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256'],
+};
+
+/**
+ *
+ * @param {string} token - openid connect token string
+ * @returns {Promise<user|null>}
+ */
+function getUser(token) {
+  if (!token) return Promise.resolve(null);
+
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, getKey, options, (err, decoded) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(decoded);
+    });
+  });
+}
+
+module.exports = getUser;
