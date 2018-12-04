9c0d4ead1eee2e4a4d268170ae0bba7f8e419b07
MrOrz <johnsonliang7@gmail.com>
2018-12-04 22:45:55 +0800

    Unify articles, replies and paragraphs search

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,76 +13,20 @@ const Query = {
     return loaders.docLoader.load({ index: 'paragraphs', id });
   },
 
+  articles(_, args, { loaders }) {
+    return resolveSearchForIndex('articles', args, loaders.searchResultLoader);
+  },
+
   paragraphs(_, args, { loaders }) {
-    const body = {
-      query: { match_all: {} },
-      size: args.first || 25,
-      from: args.skip || 0,
-      sort: (args.sort || []).map(({ by, order }) => ({
-        [by]: { order: (order || 'DESC').toLowerCase() },
-      })),
-    };
-
-    if (args.filter) {
-      const must = [];
-
-      if (args.filter.inText) {
-        must.push({
-          more_like_this: {
-            fields: ['text'],
-            like: args.filter.inText,
-            min_term_freq: 1,
-            min_doc_freq: 1,
-            max_query_terms: 25,
-          },
-        });
-      }
-
-      if (args.filter.contain) {
-        must.push({
-          simple_query_string: {
-            query: args.filter.contain,
-            fields: ['text'],
-          },
-        });
-      }
-
-      body.query = { bool: { must } };
-    }
-    return loaders.searchResultLoader.load({
-      index: 'paragraphs',
-      body,
-    });
+    return resolveSearchForIndex(
+      'paragraphs',
+      args,
+      loaders.searchResultLoader
+    );
   },
 
   replies(_, args, { loaders }) {
-    const body = {
-      query: { match_all: {} },
-      size: args.first || 25,
-      from: args.skip || 0,
-      sort: (args.sort || []).map(({ by, order }) => ({
-        [by]: { order: (order || 'DESC').toLowerCase() },
-      })),
-    };
-
-    if (args.filter) {
-      const must = [];
-
-      if (args.filter.contain) {
-        must.push({
-          simple_query_string: {
-            query: args.filter.contain,
-            fields: ['text'],
-          },
-        });
-      }
-
-      body.query = { bool: { must } };
-    }
-    return loaders.searchResultLoader.load({
-      index: 'replies',
-      body,
-    });
+    return resolveSearchForIndex('replies', args, loaders.searchResultLoader);
   },
 
   async paragraphReplies(_, { first = 10, skip = 0 }) {
@@ -101,5 +45,50 @@ const Query = {
     return userPromise;
   },
 };
+
+/**
+ *
+ * @param {string} index
+ * @param {object} args
+ * @param {object} searchResultLoader
+ */
+function resolveSearchForIndex(index, args, searchResultLoader) {
+  const body = {
+    query: { match_all: {} },
+    size: args.first || 25,
+    from: args.skip || 0,
+    sort: (args.sort || []).map(({ by, order }) => ({
+      [by]: { order: (order || 'DESC').toLowerCase() },
+    })),
+  };
+
+  if (args.filter) {
+    const must = [];
+
+    if (args.filter.inText) {
+      must.push({
+        more_like_this: {
+          fields: ['text'],
+          like: args.filter.inText,
+          min_term_freq: 1,
+          min_doc_freq: 1,
+          max_query_terms: 25,
+        },
+      });
+    }
+
+    if (args.filter.contain) {
+      must.push({
+        simple_query_string: {
+          query: args.filter.contain,
+          fields: ['text'],
+        },
+      });
+    }
+
+    body.query = { bool: { must } };
+  }
+  return searchResultLoader.load({ index, body });
+}
 
 module.exports = Query;
